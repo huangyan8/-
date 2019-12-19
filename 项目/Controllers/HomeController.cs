ad4b5cb47e1289bafd83bb8bcc5e78ca7d83e22a
2497265994@qq.com <2497265994@qq.com>
2019-12-19 16:50:02 +0800

    更改

---
+++ 项目/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace 项目.Controllers
+{
+    public class HomeController : ApiController
+    {
+    }
+}
---
+++ 项目/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="项目.MvcApplication" Language="C#" %>
